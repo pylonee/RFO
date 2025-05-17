d8c3f4705596fe46749b8649d98f53dbccaa8058
pylonee <senseiofpain@gmail.com>
2025-05-17 04:22:22 +0400

    Delete handlers directory

--- handlers/admin_panel/statistics.py
+++
@@ -1,9 +0,0 @@
-# получение статистики
-
-from telegram import Update
-from telegram.ext import CallbackContext
-from keyboards.buttons import ButtonsName
-
-async def show_stats(update: Update, context: CallbackContext) -> None:
-    await update.message.reply_text(f"{ButtonsName.STATISTIC}:")
-    await update.message.reply_text(f"1. AAA\n 2. SSS\n 3. DDD")
